f225bed5c1e63688ca6787a8fdc00f0baa1bc216
Philippe B <root@localho.st>
2018-04-20 17:21:38 +0200

    Initial commit : The DietPi config file and a script that will enable both SPI outputs on a RasPi.

---
+++ setup_spi_pi.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This script is launched only once, during DietPi automated installation.
+# It automatically configures DietPi on a RasPi 3(B) to be used with TheBigLEDowSPI
+# Allowing a RasPi to control 2 x 5000+ SPI (APA102/SK9822) LED arrays or strips at 25 FPS or more
+# The less LEDs you have, the more FPS you get... below 25 FPS, your animation won't look good...
+# but if you don't care (ie: no animation), you can drive tens of thousands of LEDs (on a single output).
+
+# man... How can you live without Vim ?
+apt -y install vim
+
+# if you are testing with the python script, you should uncomment the following line
+#apt -y install pypy
+
+# save power
+sed -i 's/exit/\/usr\/bin\/tvservice -o\n\nexit/g' /etc/rc.local
+# don't mess with the SPI hardware clock
+sed -i 's/exit/echo "performance" > \/sys\/devices\/system\/cpu\/cpu0\/cpufreq\/scaling_governor\n\nexit/g' /etc/rc.local
+
+# set SPI HW buffer size to 20K (5120 APA102/SK9822 SPI LEDs... the maximum doable by the LEDs to keep FPS above 25)
+sed -i '$ s/$/ spidev.bufsiz=20480/g' cmdline.txt
+# disable serial console (UART) so we can enable SPI1
+sed -i 's/console=serial0,115200 //g' cmdline.txt
+
+# disable bad or useless things
+cat << EOF > /etc/modprobe.d/raspi-blacklist.conf
+## bt
+blacklist btbcm
+blacklist bluetooth
+blacklist hci_uart
+## wifi
+blacklist cfg80211
+blacklist ecdh_generic
+## FW
+blacklist ip_tables
+blacklist x_tables
+EOF
+
+# disable BT and enable SPI 0 and 1
+echo /DietPi/config.txt /boot/config.txt | tr ' ' '\n' | while read thisfile; do
+if [ -f $thisfile ]; then
+sed -i 's/dtparam=spi=off/dtparam=spi=on/g' $thisfile
+cat << EOF >> $thisfile
+####### NEW (enable SPI1)
+dtoverlay=pi3-disable-bt
+enable_uart=0
+dtparam=uart0=off
+dtparam=uart1=off
+dtoverlay=spi1-1cs
+dtoverlay=spi0-hw-cs
+EOF
+fi
+done
+
+exit 0
+
