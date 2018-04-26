c32172989ad4cb986833380579951aff2ef05a89
philippebourcier <root@localho.st>
2018-04-26 19:48:00 +0200

    Fix rc.local and create a supervisor alike startup script

---
+++ launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while [ 1 ]; do
+
+# ugly quickfix
+conn=`netstat -an | grep 127.0.0.1:4200 | grep -c CLOSE_WAIT`
+if [ $conn -gt 0 ]; then
+	pkill TheBigLEDowSPI || true
+fi
+
+# launching the program
+zps=`pgrep -c TheBigLEDowSPI`
+if [ $zps -lt 1 ] ; then
+        /usr/local/bin/TheBigLEDowSPI 127.0.0.1 4200 /dev/spidev0.0 4 3000 &>/var/log/leds.log &
+        chrt -f -p 99 $!
+fi
+
+# restart after 120s upon crash, just in case :)
+sleep 120
+
+done
+
+
