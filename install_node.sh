898640751204526f8a947699a80c75bb18a068a4
philippebourcier <root@localho.st>
2018-11-29 15:39:06 +0100

    remove mdns

--- install_node.sh
+++ install_node.sh
@@ -7,5 +7,5 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 # apt-get install -y nodejs build-essential
 apt-get install -y nodejs
 
-npm -g install express socket.io body-parser mdns
+npm -g install express socket.io body-parser #mdns
 
