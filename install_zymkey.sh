4013a7d555faccb72b5119cb7119f641d493e841
PhilippeB_Maker <33067321+philippebourcier@users.noreply.github.com>
2019-12-02 22:34:26 +0100

    Create install_zymkey.sh

---
+++ install_zymkey.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/DietPi/dietpi/func/dietpi-set_hardware i2c enable
+
+apt install build-essential
+pip install wheel
+pip3 install wheel
+
+curl -G https://s3.amazonaws.com/zk-sw-repo/install_zk_sw.sh | sudo bash
