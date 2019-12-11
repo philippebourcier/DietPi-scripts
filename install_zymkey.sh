a2e74746c25b05617c108aa33e42416b369ef1a5
PhilippeB_Maker <33067321+philippebourcier@users.noreply.github.com>
2019-12-11 17:14:23 +0100

    Update install_zymkey.sh

--- install_zymkey.sh
+++ install_zymkey.sh
@@ -3,7 +3,7 @@
 /DietPi/dietpi/func/dietpi-set_hardware i2c enable
 
 apt -y install build-essential
-pip install wheel
+apt -y install python3
 pip3 install wheel
 
 curl -G https://s3.amazonaws.com/zk-sw-repo/install_zk_sw.sh | sudo bash
