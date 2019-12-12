13f50ccb93e864f2d8691816bec150ab676e812d
PhilippeB_Maker <33067321+philippebourcier@users.noreply.github.com>
2019-12-12 17:47:19 +0100

    Update setup_pi.sh

--- setup_pi.sh
+++ setup_pi.sh
@@ -39,7 +39,7 @@ sed -i 's/PermitRootLogin/#PermitRootLogin/g' /etc/ssh/sshd_config
 echo -e "PasswordAuthentication no\nPermitRootLogin prohibit-password\n" >> /etc/ssh/sshd_config
 service sshd restart
 
-# update boot wait to 10s max
+# update boot wait to 0s
 sed -i "/^CONFIG_BOOT_WAIT_FOR_NETWORK=/c CONFIG_BOOT_WAIT_FOR_NETWORK=0" /DietPi/dietpi.txt
 
 exit 0
