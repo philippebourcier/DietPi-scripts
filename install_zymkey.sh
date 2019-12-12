dcc8972e438bf8c6e5c060698f87c8b80b0c1d2f
PhilippeB_Maker <33067321+philippebourcier@users.noreply.github.com>
2019-12-12 15:55:59 +0100

    Update install_zymkey.sh

--- install_zymkey.sh
+++ install_zymkey.sh
@@ -7,10 +7,4 @@ apt -y install python3 python3-pip python-pip
 pip install wheel
 pip3 install wheel
 
-wget https://s3.amazonaws.com/zk-sw-repo/mk_encr_ext_rfs.sh
-# TODO : mod script
-# TODO : launch at next startup
-
 curl -G https://s3.amazonaws.com/zk-sw-repo/install_zk_sw.sh | bash
-#reboot
-
