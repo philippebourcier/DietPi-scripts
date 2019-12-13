33bd91a186fe1ec0726ae76bb1e2fa6b6bfb135f
PhilippeB_Maker <33067321+philippebourcier@users.noreply.github.com>
2019-12-13 17:16:55 +0100

    Update setup_luks_zymkey_dietpi.sh

--- setup_luks_zymkey_dietpi.sh
+++ setup_luks_zymkey_dietpi.sh
@@ -6,6 +6,11 @@ cd /root/
 # get the zymkey script
 wget https://s3.amazonaws.com/zk-sw-repo/mk_encr_ext_rfs.sh
 
+#
+OFDISK=`grep fdisk mk_encr_ext_rfs.sh | grep EXT_DEV | awk '{ print $3 }'`
+NFDISK='"n\np\n2\n\n+1G\n\nn\ne\n${RFS_DST_PART_NUM}\n\n${MAX_RFS_SIZE}\n\nt\n2\ne\nw\n"'
+sed -i "s/${OFDISK//\\n/\\\\n}/${NFDISK//\\n/\\\\n}/g" mk_encr_ext_rfs.sh
+
 # copy config.txt to /DietPi/
 sed -i 's/>> \/mnt\/tmpboot\/config.txt/& \&\& \/bin\/cp -f \/mnt\/tmpboot\/config.txt \/DietPi\//g' mk_encr_ext_rfs.sh
 
